3eb0bc5389ade372b96f9ba2d5bbdf993576a10c
akshay-yeole <akshayyeole1@outlook.com>
2024-02-10 20:02:51 +0530

    Add project files.

---
+++ RealTimeCharts.Server/Program.cs
@@ -0,0 +1,36 @@
+using RealTimeCharts.Server.HubConfig;
+using RealTimeCharts.Server.TimerFeatures;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder => builder
+        .WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials());
+});
+builder.Services.AddSignalR();
+
+builder.Services.AddSingleton<TimerManager>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("CorsPolicy");
+app.UseAuthorization();
+app.MapControllers();
+app.MapHub<ChartHub>("/chart");
+
+app.Run();
